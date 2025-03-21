8bb484ae9d7867836907cf8f716568ee2323cfaf
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-22 00:46:50 +0300

    context -In depth

--- index.js
+++ index.js
@@ -9,15 +9,75 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+// const allWagesFor = function () {
+//     const eligibleDates = this.timeInEvents.map(function (e) {
+//         return e.date
+//     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+//     const payable = eligibleDates.reduce(function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+//     return payable
+// }
+
+
+
+// Create Employee Record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
 }
 
+// Create Employee Records from Arrays
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+}
+
+// Add Time In Event
+function createTimeInEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+// Add Time Out Event
+function createTimeOutEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+// Hours Worked on a Given Date
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+// Wages Earned on a Given Date
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+// All Wages for Employee
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(e => e.date);
+    return eligibleDates.reduce((memo, d) => memo + wagesEarnedOnDate.call(this, d), 0);
+};
+
+// Find Employee by First Name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName);
+}
+
+// Calculate Payroll
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, record) => total + allWagesFor.call(record), 0);
+}
